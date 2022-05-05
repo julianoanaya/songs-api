8670fa2fbc3c210d8e24a05b6fdf1506f7262b18
Julian Anaya <juliananaya531@gmail.com>
2022-05-05 11:58:27 -0700

    added create action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,4 +7,15 @@ class SongsController < ApplicationController
         song = Song.find(params["id"])
         render json: song.as_json
     end
+    def create
+        song = Song.new(
+            title: params["title"],
+            album: params["album"],
+            artist: params["artist"],
+            year: params["year"]
+
+        )
+        song.save
+        render json: song.as_json
+    end
 end
