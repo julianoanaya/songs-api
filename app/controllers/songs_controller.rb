f299b4a567b4599bb381d4a74abebb6470eceaf4
Julian Anaya <juliananaya531@gmail.com>
2022-05-05 12:10:50 -0700

    added update action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,4 +18,15 @@ class SongsController < ApplicationController
         song.save
         render json: song.as_json
     end
+    def update
+        song_id = params["id"]
+        song = Song.find_by(id:song_id)
+        song.title = params[:title] || song.title
+        song.album = params[:album] || song.album
+        song.artist = params[:artist] || song.artist
+        song.year = params[:year] || song.year
+        song.save
+
+        render json: song.as_json
+    end
 end
